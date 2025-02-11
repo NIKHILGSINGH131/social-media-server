ff801684db0ed8d4e19a9c8eab5c1d5cddc302da
NIKHILGSINGH131 <you@example.com>
2025-02-11 22:15:51 +0530

    edited allawed origins in index.js of server

--- index.js
+++ index.js
@@ -25,18 +25,34 @@ app.use(express.json({limit:'10mb'}));
 app.use(morgan("common"));
 app.use(cookieParser());
 
-let origin='http://localhost:3000';
-console.log('here env',process.env.NODE_ENV);
-if(process.env.NODE_ENV === 'production'){
-  origin=process.env.CORS_ORIGIN;
-}
+// let origin='http://localhost:3000';
+// console.log('here env',process.env.NODE_ENV);
+// if(process.env.NODE_ENV === 'production'){
+//   origin=process.env.CORS_ORIGIN;
+// }
 
+
+// app.use(cors({
+//   credentials:true,
+//   // origin:process.env.CORS_ORIGIN
+//   origin
+// }))     
+
+const allowedOrigins = [
+  "http://localhost:3000", 
+  "https://client-app-6eix9.ondigitalocean.app" // Your deployed frontend
+];
 
 app.use(cors({
-  credentials:true,
-  // origin:process.env.CORS_ORIGIN
-  origin
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 app.use("/auth", authRouter);
 app.use("/posts", postsRouter);
